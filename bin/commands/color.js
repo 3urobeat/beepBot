3d38c548be80746d5b30b32e4010ee0d4856359f
HerrEurobeat <HerrEurobeat@users.noreply.github.com>
2018-02-27 19:44:19 +0100

    Version 2.11.1 stable
    See changelog in changelog.txt!

--- bin/commands/color.js
+++ bin/commands/color.js
@@ -19,14 +19,44 @@ module.exports.run = async (bot, message, args) => {
                 message.channel.send("Please remove any #'s.")
                 return
             }
+            var wantedcolor = args[0]
+            switch(args[0].toLowerCase()) {
+                case "colors":
+                    message.channel.send("Supported 'word' colors: black, gray, red, green, blue, yellow, purple, gold, cyan, white, pink.\nDisplay your color with *color (hex code).\nGet a random color by just typing *color")
+                    return;
+                case "black":
+                    var wantedcolor = 0x000000
+                case "gray":
+                    var wantedcolor = 0x808080
+                case "red":
+                    var wantedcolor = 0xFF0000
+                case "green":
+                    var wantedcolor = 0x00FF00
+                case "blue":
+                    var wantedcolor = 0x0000FF
+                case "yellow":
+                    var wantedcolor = 0xFFFF00
+                case "purple":
+                    var wantedcolor = 0x800080
+                case "gold":
+                    var wantedcolor = 0xFFD700
+                case "cyan":
+                    var wantedcolor = 0x00FFFF
+                case "white":
+                    var wantedcolor = 0xFFFFFF
+                case "pink":
+                    var wantedcolor = 0xFFC0CB
+
+            }
+            
             message.channel.send({embed:{
                 author: {
                     name: index.BOTNAME,
                     icon_url: bot.user.avatarURL,
                     url: v.botinvitelink
                 },
-                description: "Your color: #" + args[0],
-                color: args[0]
+                description: "Your color: #" + wantedcolor,
+                color: wantedcolor
                 }}).catch(err => {
                     message.channel.send("Error: " + err)
                     return;
