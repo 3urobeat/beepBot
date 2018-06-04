dff684dc27b6e4108dc966861405276b3f41d083
HerrEurobeat <HerrEurobeat@users.noreply.github.com>
2018-06-04 14:53:13 +0200

    Version 2.13.6 stable
    See changelog in changelog.txt!

--- bin/commands/userinfo.js
+++ bin/commands/userinfo.js
@@ -19,11 +19,17 @@ module.exports.run = async (bot, message, args) => {
         var usermentioninfo = message.mentions.members.first().user
         var usermentiontext = "@" + message.mentions.members.first().user.username
     }
+
+    if (message.guild.afkChannel != undefined) { 
+        var afktimeout = message.guild.afkTimeout / 60 + " min"
+    } else {
+        var afktimeout = " /"
+    }
     
     message.channel.send(index.BOTNAME + " Version " + v.BOTVERSION + " made by " + v.BOTOWNER + ". My Server: `discord.gg/q3KXW2P`");
     var botinfocontent = "\n**Bot Username:** " + v.bot.user.username + "\n**Bot ID:** " + v.bot.user.id + "\n**Bot Owner:** " + v.BOTOWNER + "\n**Bot Uptime:** " + v.round(v.bot.uptime / 3600000, 2) + " hours" + "\n**Discord.js Version:** " + v.Discord.version + "\n**Bot Heartbeat:** " + v.round(v.bot.ping, 2) + "ms" + "\n**Memory Usage:** " + (process.memoryUsage().heapUsed / 1024 / 1024).toFixed(2) + " MB\n**Servers:** " + v.bot.guilds.size + "\n**User Amount:** " + v.bot.users.size + "\n**Invite me:** [Click here!](" + index.botinvite + ")"
     var userinfocontent = "\n**Nickname:** " + usermentioninfo.username + "\n**Tag:** #" + usermentioninfo.discriminator + "\n**User ID:** " + usermentioninfo.id + "\n**Created At:** " + message.member.user.createdAt + "\n**Avatar:** [Click here!](" + message.member.user.avatarURL + ")"
-    var serverinfocontent = "\n**Name:** " + message.guild.name + "\n**Server ID:** " + message.guild.id + "\n**Server Owner:** " + message.guild.owner.user.username + "#" + message.guild.owner.user.discriminator + "\n**Created At:** " + message.guild.createdAt + "\n**User Count:** " + message.guild.memberCount + "\n**Channel Name:** #" + message.channel.name + "\n**Channel Count:** " + message.guild.channels.size + "\n**Channel ID:** " + message.channel.id + "\n**AFK Timeout:** " + message.guild.afkTimeout / "60" + " min\n**Server Icon:** get it with *servericon"
+    var serverinfocontent = "\n**Name:** " + message.guild.name + "\n**Server ID:** " + message.guild.id + "\n**Server Owner:** " + message.guild.owner.user.username + "#" + message.guild.owner.user.discriminator + "\n**Created At:** " + message.guild.createdAt + "\n**User Count:** " + message.guild.memberCount + "\n**Channel Name:** #" + message.channel.name + "\n**Channel Count:** " + message.guild.channels.size + "\n**Channel ID:** " + message.channel.id + "\n**AFK Timeout:** " + afktimeout + " \n**Server Icon:** get it with *servericon"
 
     if (infotype === "bot") {
         botinfo();
