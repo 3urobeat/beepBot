0cccee44dd84ff20dd3b23586c8135ac1cf7cc73
HerrEurobeat <HerrEurobeat@users.noreply.github.com>
2018-07-31 23:26:08 +0200

    Version 2.13.9 stable fix
    See changelog in changelog.txt!

--- bin/commands/wikihow.js
+++ bin/commands/wikihow.js
@@ -1,7 +1,7 @@
 module.exports.run = async (bot, message, args) => {
     const v = require("../vars.js")
     
-    message.channel.send("Random WikiHow Article: \nhttps://de.wikihow.com/Special:Randomizer")
+    message.channel.send("Random WikiHow Article: \nhttps://en.wikihow.com/Special:Randomizer")
 
 }
 
