5fb59a650afc79694d67fdc448b4e67bb01f1f7f
HerrEurobeat <HerrEurobeat@users.noreply.github.com>
2018-10-02 13:04:43 +0200

    Version 2.13.12 stable
    See changelog in changelog.txt!

--- bin/commands/help.js
+++ bin/commands/help.js
@@ -2,31 +2,9 @@ module.exports.run = async (bot, message, args) => {
     const v = require("../vars.js")
     const index = require("../index.js") 
 
-    const help = v.fs.readFileSync(v.helppath, {"encoding": "utf-8"});
-    const help2 = v.fs.readFileSync(v.helppath2, {"encoding": "utf-8"});
-    const help3 = v.fs.readFileSync(v.helppath3, {"encoding": "utf-8"});
-    const help4 = v.fs.readFileSync(v.helppath4, {"encoding": "utf-8"});
-    const help5 = v.fs.readFileSync(v.helppath5, {"encoding": "utf-8"});
-    const help6 = v.fs.readFileSync(v.helppath6, {"encoding": "utf-8"});
-    
-    try {
-        await message.author.send("\n" + help)
-        await message.author.send("\n" + help2)
-        await message.author.send("\n" + help3 + "\n" + help4)
-        await message.author.send("\n" + help5 + "\n" + help6)
-        await message.author.send("Invite me to your server: ``" + index.botinvite + "``\nYou need help? English help and german chat: https://discord.gg/" + v.ssinvitecode)
-    } catch (err) {
-        message.channel.send("An error occured sending you a DM with a list of all my commands, including a link to my support server. \nRead all commands here: https://github.com/HerrEurobeat/beepBot/blob/master/bin/help/helptext.txt \nIf you need help type *invite to get an invite to my server.").catch(err => {
-            console.log("help send error message error: " + err)
-        })
-        return;
-    }
-
-    if (message.channel.type == "dm") {
-        return;
-    } else {
-        await message.channel.send(v.randomstring(['A list of all commands was send to you per DM. :mailbox_with_mail: :poop:','Ding! You have got an DM! :mailbox_with_mail: ','DM or Mail? I think DM is better. Here you go. :mailbox_with_mail: ','Here you go!','Have fun!','Spam is real!','Sorry, i did not spam you. Discord just does not let me post long messages.']))
-    }
+    message.channel.send("Read all commands here: https://github.com/HerrEurobeat/beepBot/blob/master/bin/help/helptext.txt \nIf you need help type *invite to get an invite to my server.").catch(err => {
+        console.log("help send error message error: " + err)
+    })
 }
 
 module.exports.config = {
--- bin/commands/info.js
+++ bin/commands/info.js
@@ -25,10 +25,16 @@ module.exports.run = async (bot, message, args) => {
     } else {
         var afktimeout = " /"
     }
+
+    try {
+        var usergame = message.member.presence.game.name
+    } catch (err) {
+        var usergame = "/"
+    }
     
     message.channel.send(index.BOTNAME + " Version " + v.BOTVERSION + " made by " + v.BOTOWNER + ". My Server: `discord.gg/" + v.ssinvitecode + "`");
-    var botinfocontent = "\n**Bot Username:** " + v.bot.user.username + "\n**Bot ID:** " + v.bot.user.id + "\n**Bot Owner:** " + v.BOTOWNER + "\n**Bot Uptime:** " + v.round(v.bot.uptime / 3600000, 2) + " hours" + "\n**Discord.js Version:** " + v.Discord.version + "\n**Bot Heartbeat:** " + v.round(v.bot.ping, 2) + "ms" + "\n**Memory Usage:** " + (process.memoryUsage().heapUsed / 1024 / 1024).toFixed(2) + " MB\n**CPU Temp:** " + tempc + " °C | " + tempf + " °F\n**Servers:** " + v.bot.guilds.size + "\n**User Amount:** " + v.bot.users.size + "\n**Invite me:** [Click here!](" + index.botinvite + ")"
-    var userinfocontent = "\n**Nickname:** " + usermentioninfo.username + "\n**Tag:** #" + usermentioninfo.discriminator + "\n**User Status:** " + usermentioninfo.presence.status + "\n**User ID:** " + usermentioninfo.id + "\n**Created At:** " + message.member.user.createdAt + "\n**Avatar:** [Click here!](" + message.member.user.avatarURL + ")"
+    var botinfocontent = "\n**Bot Username:** " + v.bot.user.username + "\n**Bot Owner:** " + v.BOTOWNER + "\n**Bot Uptime:** " + v.round(v.bot.uptime / 3600000, 2) + " hours" + "\n**Discord.js Version:** " + v.Discord.version + "\n**Bot Heartbeat:** " + v.round(v.bot.ping, 2) + "ms" + "\n**Memory Usage:** " + (process.memoryUsage().heapUsed / 1024 / 1024).toFixed(2) + " MB\n**CPU Temp:** " + tempc + " °C | " + tempf + " °F\n**Servers:** " + v.bot.guilds.size + "\n**User Amount:** " + v.bot.users.size + "\n**Invite me:** [Click here!](" + index.botinvite + ")"
+    var userinfocontent = "\n**Nickname:** " + usermentioninfo.username + "\n**Tag:** #" + usermentioninfo.discriminator + "\n**User Status:** " + usermentioninfo.presence.status + "\n**User Game:** " + usergame + "\n**User ID:** " + usermentioninfo.id + "\n**Created At:** " + message.member.user.createdAt + "\n**Avatar:** [Click here!](" + message.member.user.avatarURL + ")"
     var serverinfocontent = "\n**Name:** " + message.guild.name + "\n**Server ID:** " + message.guild.id + "\n**Server Owner:** " + message.guild.owner.user.username + "#" + message.guild.owner.user.discriminator + "\n**Created At:** " + message.guild.createdAt + "\n**User Count:** " + message.guild.memberCount + "\n**Channel Name:** #" + message.channel.name + "\n**Channel Count:** " + message.guild.channels.size + "\n**Channel ID:** " + message.channel.id + "\n**AFK Timeout:** " + afktimeout + " \n**Server Icon:** get it with *servericon"
 
     if (infotype === "bot") {
